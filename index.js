acc0d7e8b1ea767fa94e746c71af6254d0206569
renaldo025 <renaldo.aldo2536@gmail.com>
2021-11-14 03:46:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import { AppRegistry } from 'react-native';
 import App from './src/navigators/navigation';
 import { name as appName } from './app.json';
+import React from 'react';
+import { Provider } from 'react-redux';
+import Store from "./src/redux/store"
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => (
+    <Provider store={Store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Root);
